8cece22c04de7f15c0560cd2537fb65a981b9251
Ridho8390 <muhamadridhoabdilah391@gmail.com>
2026-05-18 14:50:34 +0700

    post factory & post seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'image' => $this->faker->imageUrl(640, 480, 'tech', true),
+
+            'title' => $this->faker->sentence(),
+
+            'content' => $this->faker->paragraphs(3, true),
+
+            'created_at' => now(),
+            
+            'updated_at' => now(),
+        ];
+    }
+}
